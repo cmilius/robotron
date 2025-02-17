845262f6d92b22e827b149c4af2401ca3cf3b526
JB <jjbecquer@gmail.com>
2025-02-16 20:40:09 -0600

    Added h_stack and v_stack to keep track of the most recently pushed button by the player.

--- scripts/entities.py
+++ scripts/entities.py
@@ -69,9 +69,47 @@ class Hero(PhysicsEntity):
         self.projectile_reload = 20  # frames
         self.projectile_timer = 10  # count up to projectile reload
 
+        # used to track the last pressed keys for shooting
+        self.h_stack = []
+        self.v_stack = []
+
     def update(self, movement=(0, 0), shooting=(False, False, False, False)):
         super().update(movement=movement)
+
         self.shooting = list(shooting)
+        # Find out which key was last pressed by the hero
+        if self.shooting[0] and "left" not in self.h_stack:
+            self.h_stack.append("left")
+        elif not self.shooting[0] and "left" in self.h_stack:
+            self.h_stack.remove("left")
+        if self.shooting[1] and "right" not in self.h_stack:
+            self.h_stack.append("right")
+        elif not self.shooting[1] and "right" in self.h_stack:
+            self.h_stack.remove("right")
+        if self.h_stack:
+            if self.h_stack[-1] == "left":
+                self.shooting[0] = True
+                self.shooting[1] = False
+            else:
+                self.shooting[0] = False
+                self.shooting[1] = True
+        if self.shooting[2] and "up" not in self.v_stack:
+            self.v_stack.append("up")
+        elif not self.shooting[2] and "up" in self.v_stack:
+            self.v_stack.remove("up")
+        if self.shooting[3] and "down" not in self.v_stack:
+            self.v_stack.append("down")
+        elif not self.shooting[3] and "down" in self.v_stack:
+            self.v_stack.remove("down")
+        if self.v_stack:
+            if self.v_stack[-1] == "up":
+                self.shooting[2] = True
+                self.shooting[3] = False
+            else:
+                self.shooting[2] = False
+                self.shooting[3] = True
+
+        # only fire if the gun is reloaded
         if self.projectile_timer != self.projectile_reload:
             self.projectile_timer += 1
         if True in self.shooting and (self.projectile_timer == self.projectile_reload):
