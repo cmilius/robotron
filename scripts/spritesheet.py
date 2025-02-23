7b466d326da88f8dcbaf329531d2f4c2884e9fca
Charlie <cmilius204@gmail.com>
2025-02-23 13:26:20 -0600

    Initial spritesheet parsing via json file input

--- scripts/spritesheet.py
+++ scripts/spritesheet.py
@@ -1,8 +1,15 @@
 import pygame
+import json
 
 class SpriteSheet:
     def __init__(self, filename):
         self.sprite_sheet = pygame.image.load(filename).convert()
+
+        # Load the meta data for the sprite sheet
+        self.meta_data = filename.replace('png', 'json')
+        with open(self.meta_data) as f:
+            self.data = json.load(f)
+        f.close()
 
     def get_sprite(self, x, y, width, height):
         """Get a single sprite from the sprite sheet.
@@ -11,21 +18,38 @@ class SpriteSheet:
         """
 
         sprite = pygame.Surface((width, height)).convert()              # define an empty surface
-        #sprite.set_colorkey((0, 0, 0))                                 # set the color key for transparency
+        sprite.set_colorkey((255, 255, 255))                            # set the color key for transparency
         sprite.blit(self.sprite_sheet, (0, 0), (x, y, width, height))   # copy the sprite from the sprite sheet to the surface
         return sprite                                                   # return the surface
     
-    def get_sprites(self, x, y, width, height, rows, cols):
+    def get_sprites(self, x, y, width, height, rows, cols, x_padding=0, y_padding=0):
         """Get a grid of sprites from the sprite sheet.
         
         :return: List of sprite surfaces
         """
 
         # TODO: Right now this only works for a grid of same-size sprites. This may need updated if the sprites in the sheet aren't uniform in size.
-        sprites = []
+        sprites = []        
         for row in range(rows):
+            #row padding
+            if row > 0:
+                    y += y_padding
+
             for col in range(cols):
+                #column padding
+                if col > 0:
+                    x += x_padding
+
                 sprite = self.get_sprite(x + col * width, y + row * height, width, height)
                 sprites.append(sprite)
         return sprites
 
+    def parse_sprites(self, entity_name):
+        """Parse the sprite sheet for a specific entity.
+        
+        :return: List of sprite surfaces
+        """
+
+        sprite = self.data[self.meta_data.rsplit('/', 1)[1]][entity_name]
+        sprite_frame = sprite['frame']
+        return self.get_sprites(sprite_frame['x'], sprite_frame['y'], sprite_frame['w'], sprite_frame['h'], sprite['rows'], sprite['cols'], sprite['padding']['x'], sprite['padding']['y'])
