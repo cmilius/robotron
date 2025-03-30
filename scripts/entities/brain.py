9e658db756f3e4502e17aa898c2bbbb696874a6b
Charlie <cmilius204@gmail.com>
2025-03-29 23:16:20 -0500

    Initial work on the Brain enemy

---
+++ scripts/entities/brain.py
@@ -0,0 +1,85 @@
+from scripts.entities.entities import PhysicsEntity
+
+class Brain(PhysicsEntity):
+    """
+        These are the most intelligent enemies. They fire Cruise Missiles that home in 
+        on the player and can capture humans, turning them into Progs. They only appear every 
+        5 levels in a "Brain Wave."
+    """
+
+    # define constants
+    POINT_VALUE     = 500  # point value for the brain
+    MOVEMENT_SPEED  = 0.2  # movement speed of the brain
+
+    def __init__(self, game, pos, size):
+        """
+        Initialize the Brain entity.
+        :param game: The game object.
+        :param pos: The position of the brain.
+        :param size: The size of the brain.
+        """
+
+        # inheret the PhysicsEntity class
+        super().__init__(game, self.__class__.__name__.lower(), pos, size)  
+        
+        self.action = "idle"        # initial stance
+        self.target_pos = [0, 0]    # target position for the brain to move to
+
+    def update(self, movement=(0, 0)):
+        """
+        Moves the brain. The brain can move in any direction, including diagonally.
+        The brain will focus colliding with the nearest human to turn them into a prog.
+        If there are no humans left, the brain will chase down the player.
+
+        :param movement: Movement in (x, y). Default (0, 0)
+        :return: None
+        """
+
+        # check if there are any humans left
+        humans = self.game.family_group
+
+        if len(humans) > 0:
+            # if there are humans left, chase the closest one
+            closest_human = min(humans, key=lambda h: self.distance_to_target(h.pos))
+            target_pos = closest_human.pos
+        else:
+            # if there are no humans left, chase the player
+            target_pos = [self.game.hero.rect[0], self.game.hero.rect[1]]
+
+        # move to the target position
+        super().move_to_target(target_pos,
+                               movement=movement,
+                               scaler=self.MOVEMENT_SPEED,
+                               move_dir=None)
+
+    def fire_projectile(self):
+        """
+        Fire cruise missile at the hero
+        :return: None
+        """
+        # TODO: Implement the fire_projectile method for the brain.
+
+    def create_prog(self):
+        """
+        Turn a human into a Prog
+        :return: None
+        """
+        # TODO: Implement the create_prog method for the brain.
+
+    def hit_by_projectile(self):
+        """
+        Brain ded
+
+        :return: None
+        """
+        self.kill()
+
+    def get_point_value(self):
+        """
+        Returns the point value of the brain
+
+        :return: int
+        """
+        # TODO: This method should be moved to the PhysicsEntity class and pull from each
+        # entity sub-class's POINT_VALUE propery. This is a placeholder for now.
+        return self.POINT_VALUE
