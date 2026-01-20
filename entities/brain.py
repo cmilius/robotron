5f279a1c8cb904eab6a4645a37813660f771fcf9
Charlie Milius <cmilius204@gmail.com>
2026-01-19 21:36:31 -0600

    Brain and Prog initial commit

---
+++ entities/brain.py
@@ -0,0 +1,101 @@
+from entities.entities import PhysicsEntity
+from entities.prog import Prog
+
+class Brain(PhysicsEntity):
+    """
+        These are the most intelligent enemies. They fire Cruise Missiles that home in 
+        on the player and can capture humans, turning them into Progs. They only appear every 
+        5 levels in a "Brain Wave."
+    """
+
+    # define constants
+    POINT_VALUE     = 500  # point value for the brain
+    MOVEMENT_SPEED  = 0.2  # movement speed of the brain
+
+    def __init__(self, game, pos, size):
+        """
+        Initialize the Brain entity.
+        :param game: The game object.
+        :param pos: The position of the brain.
+        :param size: The size of the brain.
+        """
+
+        # inheret the PhysicsEntity class
+        super().__init__(game, self.__class__.__name__.lower(), pos, size)  
+        self.image = self.game.robotrons_animations.animations[self.e_type][self.action][0]
+        self.action = "idle"        # initial stance
+        self.target_pos = [0, 0]    # target position for the brain to move to
+
+    def update(self, movement=(0, 0)):
+        """
+        Moves the brain. The brain can move in any direction, including diagonally.
+        The brain will focus colliding with the nearest human to turn them into a prog.
+        If there are no humans left, the brain will chase down the player.
+
+        :param movement: Movement in (x, y). Default (0, 0)
+        :return: None
+        """
+
+        # chase the player
+        target_pos = [self.game.hero.rect[0], self.game.hero.rect[1]]
+        
+        # unless there are humans left, chase the closest one
+        humans = self.game.family_group.sprites()
+
+        if len(humans) > 0:
+            
+            # initialize the closest human position
+            smallest_distance = 0
+
+            # loop through the humans to find the closest one
+            for human in humans:
+                x = self.pos[0] - human.pos[0]
+                y = self.pos[1] - human.pos[1]
+                distance = abs(x) + abs(y)
+
+                # Update the smallest distance and target position if a closer human is found
+                if smallest_distance == 0 or distance <= smallest_distance:
+                    smallest_distance = distance
+                    target_pos = human.pos       
+
+        # move to the target position
+        super().move_to_target(target_pos,
+                               movement=movement,
+                               scaler=self.MOVEMENT_SPEED,
+                               move_dir=None)
+
+    def fire_projectile(self):
+        """
+        Fire cruise missile at the hero
+        :return: None
+        """
+        # TODO: Implement the fire_projectile method for the brain.
+
+    def spawn_prog(self):
+        """
+        Turn a human into a Prog
+        :return: None
+        """
+
+        # spawn the prog at the brain's position
+        prog = Prog(self.game, (self.pos[0], self.pos[1]), self.game.prog_size)
+        self.game.enemy_group.add(prog)
+        self.game.allsprites.add(prog)
+
+    def hit_by_projectile(self):
+        """
+        Brain ded
+
+        :return: None
+        """
+        self.kill()
+
+    def get_point_value(self):
+        """
+        Returns the point value of the brain
+
+        :return: int
+        """
+        # TODO: This method should be moved to the PhysicsEntity class and pull from each
+        # entity sub-class's POINT_VALUE propery. This is a placeholder for now.
+        return self.POINT_VALUE
---
+++ entities/prog.py
@@ -0,0 +1,67 @@
+import pygame
+from entities import PhysicsEntity
+import logging
+
+logger = logging.getLogger(__name__)
+
+class Prog(PhysicsEntity):
+    """
+    Former humans who have been reprogrammed by Brains. They chase the player with deadly intent and move like fast grunts.
+    """
+
+    def __init__(self, game, pos, size):
+        super().__init__(game, self.__class__.__name__.lower(), pos, size)
+        self.animations = self.game.human_family_animations.animations[self.e_type] # they were originally humans
+        self.image = self.animations[self.action][0] 
+
+        self.target_posit = self.random_movement()
+
+        self.block_actions = True  # block movement until the prog has fully spawned
+        self.spawn_frames = 6  # the number of animations frames in the prog spawn
+        self.frame_counter = 0
+        self.anim_frame_delay = 0
+        self.trail_images = []  # list to store the trail images
+
+
+    def animate(self, frame_movement):
+        """
+        Given a list frame_movement, update the entity animations.
+
+        :param list frame_movement: The direction vector the entity is moving in.
+        :return: None
+        """
+
+        # Append the current image and position to the trail
+        self.trail_images.append((self.image, self.rect.topleft))
+
+        # Limit the trail to the last 15 frames
+        if len(self.trail_images) > 15:
+            self.trail_images.pop(0)
+
+        # Draw trail images at their respective positions
+        for i, (trail_image, trail_pos) in enumerate(self.trail_images):
+            if i % 5 == 0:  # Only draw every 5th image
+                self.game.display.blit(trail_image, trail_pos, special_flags=pygame.BLEND_SUB)
+
+        # Iterate the animation frames
+        super().iterate_animation_frames()
+        
+        if frame_movement[0] > 0:
+            self.action = "walk_right"
+        elif frame_movement[0] < 0:
+            self.action = "walk_left"
+        if frame_movement[1] > 0 and not frame_movement[0]:
+            self.action = "walk_down"
+        elif frame_movement[1] < 0 and not frame_movement[0]:
+            self.action = "walk_up"
+                
+        self.image = self.game.human_family_animations.animations[self.e_type][self.action][self.anim_flipbook[self.flipbook_index]]
+
+    def update(self, movement=(0, 0)):
+        # if reached its target, calculate a new target
+        self.target_posit = self.reached_target(target_pos=self.target_posit)
+
+        super().move_to_target(target_pos=self.target_posit,
+                            movement=movement,
+                            scaler=.7,
+                            move_dir=None)
