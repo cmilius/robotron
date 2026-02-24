a39399b1a680a2382b4e304ad0d01169ba9dd93e
Charlie Milius <cmilius204@gmail.com>
2026-02-23 20:50:38 -0600

    Build automation workflow improvements (#90)
    * Introducing audio into the game
    
    * Added human death sound
    
    * Adding additional game sounds
    
    * Updated deployment workflow for automation
    
    * Audio files updated to adress pybag compatibility issues
    
    * Fix artifact folder deploy target
    
    * Add repository checkout action to deployment job
    
    * Update deploy job restrictions
    
    ---------
    
    Co-authored-by: Jordan Becquer <109747613+JBecquer@users.noreply.github.com>

--- audio.py
+++ audio.py
@@ -1,3 +1,4 @@
+import sys
 import pygame
 
 # CONSTANTS
@@ -12,6 +13,24 @@ class Audio:
     def __init__(self):
         pygame.mixer.init()
 
+        if sys.platform == "emscripten":
+            extension = ".ogg"
+        else:
+            extension = ".wav"
+
+        self.sounds = {
+            "enforcer_fire": pygame.mixer.Sound(AUDIO_FILEPATH + "enforcer_fire" + extension),
+            "grunt_walk": pygame.mixer.Sound(AUDIO_FILEPATH + "grunt_walk" + extension),
+            "hero_death": pygame.mixer.Sound(AUDIO_FILEPATH + "hero_death" + extension),
+            "hero_lazer": pygame.mixer.Sound(AUDIO_FILEPATH + "hero_lazer" + extension),
+            "human_die": pygame.mixer.Sound(AUDIO_FILEPATH + "human_die" + extension),
+            "human_save": pygame.mixer.Sound(AUDIO_FILEPATH + "human_save" + extension),
+            "level_transition": pygame.mixer.Sound(AUDIO_FILEPATH + "level_transition" + extension),
+            "prog_transformation": pygame.mixer.Sound(AUDIO_FILEPATH + "prog_transformation" + extension),
+            "quark_spawn": pygame.mixer.Sound(AUDIO_FILEPATH + "quark_spawn" + extension),
+            "tank_explode": pygame.mixer.Sound(AUDIO_FILEPATH + "tank_explode" + extension),
+            "tank_fire": pygame.mixer.Sound(AUDIO_FILEPATH + "tank_fire" + extension),
+            "tank_projectile_bounce": pygame.mixer.Sound(AUDIO_FILEPATH + "tank_projectile_bounce" + extension)
         self.sounds = {
             "enforcer_fire": pygame.mixer.Sound(AUDIO_FILEPATH + "enforcer_fire.wav"),
             "grunt_walk": pygame.mixer.Sound(AUDIO_FILEPATH + "grunt_walk.wav"),
